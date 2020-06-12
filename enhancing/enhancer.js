65c91c787d8f5f45e117956780efa16414935619
abhitsahota <abhit.sahota@gmail.com>
2020-06-11 23:21:03 -0400

    completed MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,16 +6,56 @@ module.exports = {
 };
 
 function succeed(item) {
+// a success(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success.
+
+// The item's enhancement increases by 1.
+// If the item enhancement level is 20, the enhancement level is not changed.
+// The durability of the item is not changed.
+
+  if (item.enhancement < 20) {
+    item.enhancement += 1
+  } 
+
   return { ...item };
 }
 
 function fail(item) {
+// a fail(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.
+
+// If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+  if (item.enhancement < 15) {
+    if (item.durability > 5) {
+      item.durability -= 5
+    } else {
+      item.durability = 0
+    }
+  } else {
+    if (item.durability > 10) {
+      item.durability -= 10
+    } else {
+      item.durability = 0
+    }
+  } 
+
+  if (item.enhancement > 16) {
+    item.enhancement -= 1
+  } 
+
   return { ...item };
 }
 
 function repair(item) {
+// a repair(item) method that accepts an item object and returns a new item with the durability restored to 100. This method is the simplest of the three and would be a good starting point on this project.
+
+  item.durability = 100
+
   return { ...item };
 }
+
+
+// Stretch
 
 function get(item) {
   return { ...item };
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,83 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('testing the video game item features', () => {
+
+    let testItem = {
+        name: 'sword',
+        durability: 50,
+        enhancement: 10
+    }
+    
+    let expected = null
+    let result = null
+
+    beforeEach(() => {
+        testItem = {
+            name: 'sword',
+            durability: 50,
+            enhancement: 10
+        }
+    
+        expected = null
+        result = null
+    })
+
+    describe('repair feature', () => {
+        it('repair to restore durability to 100', () => {
+            expected = 100
+            result = enhancer.repair(testItem)
+            expect(result.durability).toBe(expected)
+        })
+    })
+
+    describe('enhancement feature', () => {
+
+        describe('success scenarios', () => {
+            it('increase the level of an item that is not maxed', () => {
+                expected = 11
+                result = enhancer.succeed(testItem)
+                expect(result.enhancement).toBe(expected)
+            })
+
+            it('try to increase the level of an item that is maxed', () => {
+                expected = 20
+                testItem.enhancement = 20
+                result = enhancer.succeed(testItem)
+                expect(result.enhancement).toBe(expected)
+            })
+        })
+
+        describe('fail scenarios', () => {
+            
+
+            it('fail to enhance a enchancement level 1-14 item, durability decreases by 5', () => {
+                testItem.enhancement = 10
+                expected = testItem.durability - 5
+                result = enhancer.fail(testItem)
+                expect(result.durability).toBe(expected)
+            })
+
+            it('fail to enhance a enchancement level 15 item', () => {
+                testItem.enhancement = 15
+                expected = testItem.durability - 10
+                result = enhancer.fail(testItem)
+                expect(result.durability).toBe(expected)
+            })
+
+            it('fail to enhance a enchancement level 17 item', () => {
+                testItem.enhancement = 17
+
+                expected = testItem.enhancement - 1
+                result = enhancer.fail(testItem)
+                expect(result.enhancement).toBe(expected)
+
+                testItem.durability = 50
+                expected = testItem.durability - 10
+                expect(result.durability).toBe(expected)
+            })
+        })
+        
+    })
+
+})
